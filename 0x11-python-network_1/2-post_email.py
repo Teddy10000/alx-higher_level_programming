7a05c192dfcc6dd95e82a97d25eab78d0a3b36fc
Teddy10000 <Tedakrong12@gmail.com>
2023-03-31 23:39:37 +0000

    Working on a task that takes arguments of email and url and uses urrlib to make a request with the parameters

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This task takes in a url and an email in the request body
+encoded and sends as parameter
+"""
+if __name__ == "__main__":
+    import urllib.parse as parse
+    import urllib.request as request
+    from sys
+    value = {'email': argv[2]}
+    data = parse.urlencode(value).encode('utf-8')
+    resp = request.Request(sys.argv[1], data)
+    with request.urlopen(resp) as response:
+        print(response.read().decode('utf-8'))
