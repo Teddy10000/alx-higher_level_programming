e736ffda1c1dc3516161038cde8a9f0f8e86d49e
Teddy10000 <Tedakrong12@gmail.com>
2023-03-31 23:23:41 +0000

    Worked a task concerning fetching a URL with a status as path

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,19 @@
-1 #!usr/bin/python3
-  2 """A script that fetches the alx intranet/status"""
-  3
-  4 import urllib.request
-  5
-  6 if __name__ == '__main__':
-  7
-  8         request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-  9
- 10         with urllib.request.urlopen(request) as response:
- 11             message = response.read()
- 12             print(format(message))
- 13             print(f"Body response:")
- 14             print(f"\t -type: {format(type(message))}")
- 15             print(f"\t - content: {message}")
- 16             print(f"\t - utf8 content: {message.decode('utf-8')}")
+ #!usr/bin/python3
+
+"""
+A script that fetches the alx intranet/status
+"""
+
+import urllib.request
+
+if __name__ == '__main__':
+    
+          request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+
+          with urllib.request.urlopen(request) as response:
+             message = response.read()
+             print(format(message))
+             print(f"Body response:")
+             print(f"\t -type: {format(type(message))}")
+             print(f"\t - content: {message}")
+             print(f"\t - utf8 content: {message.decode('utf-8')}")
