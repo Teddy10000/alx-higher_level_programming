5bc85096cd33014c9462753b723c1bbcec8b076f
Teddy10000 <Tedakrong12@gmail.com>
2023-04-03 17:44:00 +0000

    Pushing the rest of the code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,9 @@
 import requests
 
 """A python code that fetches status using requests"""
-url = "https://alx-intranet.hbtn.io/status"
+
 if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
     print("Body response:")
     print("\t - type: {}".format(type(r.text)))
