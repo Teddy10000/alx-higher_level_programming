e6e9f1a936554c0f172b9e79090cd352d8f8c657
Teddy10000 <Tedakrong12@gmail.com>
2023-04-03 13:47:17 +0000

    This is an assignment concerning that fetches an alx-status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
-import sys, urllib
+import sys
+import requests
+
+"""A python code that fetches status using requests"""
+url = "https://alx-intranet.hbtn.io/status"
+if __name__ == "__main__":
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
