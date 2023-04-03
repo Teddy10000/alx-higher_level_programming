683fce0c7adfda8ba77d708de4f2906a4a7626d5
Teddy10000 <Tedakrong12@gmail.com>
2023-04-03 17:48:52 +0000

    Pushing the rest of the code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-import requests
-
-"""A python code that fetches status using requests"""
+"""A python code that fetches status using requests
+   and displays the result and the text of the content
+"""
 
 if __name__ == "__main__":
+    import requests
     url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
     print("Body response:")
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-import requests
 
-"""A python scripts that sends a post request to a url """
+"""A python scripts that sends a post request to a url
+and takes the url and sends to the email to check"""
 
 if __name__ == "__main__":
+    import sys
+    import requests
     url = sys.argv[1]
     email = sys.argv[2]
     data = {"email": email}
