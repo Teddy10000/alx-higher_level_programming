21db355fa3339f7abd1664381e31da12c18aa37e
Teddy10000 <Tedakrong12@gmail.com>
2023-04-05 16:25:26 +0000

    File to open and read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as doc:
         print(doc.read(), end="")
