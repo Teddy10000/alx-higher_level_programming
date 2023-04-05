122b6b1886ccbbb2ece1981803ce71fa6a6fbc15
Teddy10000 <Tedakrong12@gmail.com>
2023-04-05 16:17:35 +0000

    File to open and read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This Fucntion opens and read the context of a file"""
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as doc:
+        print(doc.read(), end="")
